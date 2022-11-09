0fe40b9395aaaafe617e3acf228880022da8a7e4
Class Account <eecs151-aaq@c111-6.eecs.berkeley.edu>
2022-11-09 13:31:52 -0800

    added memory_io module and updated cpu module

---
+++ hardware/src/memory_io.v
@@ -0,0 +1,78 @@
+/* IO File
+
+*/
+`timescale 1ns/1ns
+
+// IO module
+module IO_MEMORY_MAP(clk, instruction, addr, uart_tx_data_in, out);
+    input [31:0] instruction;
+    input [31:0] addr;
+    input [7:0] uart_tx_data_in;
+    output reg [31:0] out;
+
+    // On-chip UART
+    //// UART Receiver
+    wire [7:0] uart_rx_data_out;
+    wire uart_rx_data_out_valid;
+    wire uart_rx_data_out_ready;
+    //// UART Transmitter
+    // wire [7:0] uart_tx_data_in;
+    wire uart_tx_data_in_valid;
+    wire uart_tx_data_in_ready;
+    uart #(
+        .CLOCK_FREQ(CPU_CLOCK_FREQ),
+        .BAUD_RATE(BAUD_RATE)
+    ) on_chip_uart (
+        .clk(clk),
+        .reset(rst),
+
+        .serial_in(serial_in),
+        .data_out(uart_rx_data_out),
+        .data_out_valid(uart_rx_data_out_valid),
+        .data_out_ready(uart_rx_data_out_ready),
+
+        .serial_out(serial_out),
+        .data_in(uart_tx_data_in),
+        .data_in_valid(uart_tx_data_in_valid),
+        .data_in_ready(uart_tx_data_in_ready)
+    );
+
+    // Cycle Counter
+    reg [31:0] cycle_counter = 0;
+    always @(posedge clk) begin
+        if (addr == 32'h80000018) cycle_counter <= 0;
+        else cycle_counter <= cycle_counter + 1;
+    end
+
+    // Instruction Counter
+    reg [31:0] instruction_counter = 0;
+    always @(posedge clk) begin
+        if (addr == 32'h80000018) cycle_counter <= 0;
+        else if (instruction == 32'b0000_0000_0000_0000_0000_0000_0001_0011) instruction_counter <= instruction_counter;
+        else instruction_counter <= instruction_counter + 1;
+    end
+
+    // for rx_data_out_ready and tx_in_valid
+    always @(*) begin
+        if (instruction[6:2] == `OPC_LOAD_5 && addr == 32'h80000004) uart_rx_data_out_ready = 1;
+        else if (instruction[6:2] == `OPC_STORE_5 && addr == 32'h80000008) uart_tx_data_in_valid = 1; // can they be 1 at same time?
+        else begin
+            uart_rx_data_out_ready = 0;
+            uart_tx_data_in_valid = 0;
+        end
+    end
+
+    // for output of UART
+    always @(*) begin
+        case(addr)
+            32'h80000000: out = {30'b0, uart_rx_data_out_valid, uart_tx_data_in_ready};
+            32'h80000004: out = {24'b0, uart_rx_data_out};
+            32'h80000010: out = cycle_counter;
+            32'h80000014: out = instruction_counter;
+            default: begin
+                out = 32'h00000000;
+            end
+        endcase
+    end
+
+endmodule
--- hardware/src/riscv_core/cpu.v
+++ hardware/src/riscv_core/cpu.v
@@ -76,30 +76,30 @@ module cpu #(
 
     // On-chip UART
     //// UART Receiver
-    wire [7:0] uart_rx_data_out;
-    wire uart_rx_data_out_valid;
-    wire uart_rx_data_out_ready;
-    //// UART Transmitter
-    wire [7:0] uart_tx_data_in;
-    wire uart_tx_data_in_valid;
-    wire uart_tx_data_in_ready;
-    uart #(
-        .CLOCK_FREQ(CPU_CLOCK_FREQ),
-        .BAUD_RATE(BAUD_RATE)
-    ) on_chip_uart (
-        .clk(clk),
-        .reset(rst),
+    // wire [7:0] uart_rx_data_out;
+    // wire uart_rx_data_out_valid;
+    // wire uart_rx_data_out_ready;
+    // //// UART Transmitter
+    // wire [7:0] uart_tx_data_in;
+    // wire uart_tx_data_in_valid;
+    // wire uart_tx_data_in_ready;
+    // uart #(
+    //     .CLOCK_FREQ(CPU_CLOCK_FREQ),
+    //     .BAUD_RATE(BAUD_RATE)
+    // ) on_chip_uart (
+    //     .clk(clk),
+    //     .reset(rst),
 
-        .serial_in(serial_in),
-        .data_out(uart_rx_data_out),
-        .data_out_valid(uart_rx_data_out_valid),
-        .data_out_ready(uart_rx_data_out_ready),
+    //     .serial_in(serial_in),
+    //     .data_out(uart_rx_data_out),
+    //     .data_out_valid(uart_rx_data_out_valid),
+    //     .data_out_ready(uart_rx_data_out_ready),
 
-        .serial_out(serial_out),
-        .data_in(uart_tx_data_in),
-        .data_in_valid(uart_tx_data_in_valid),
-        .data_in_ready(uart_tx_data_in_ready)
-    );
+    //     .serial_out(serial_out),
+    //     .data_in(uart_tx_data_in),
+    //     .data_in_valid(uart_tx_data_in_valid),
+    //     .data_in_ready(uart_tx_data_in_ready)
+    // );
 
     reg [31:0] tohost_csr = 0;
 
@@ -138,7 +138,7 @@ module cpu #(
 	// 1.3: pc_register
 	wire [31:0] pc_register_d;
     reg [31:0] pc_register_q;
-    always(@posedge clk) begin
+    always @(posedge clk) begin
         pc_register_q <= pc_register_d;
     end
 
@@ -165,13 +165,33 @@ module cpu #(
 	);
 
 	wire nop_mux_sel;
-  	wire [31:0] nop_mux_in0, nop_mux_in1; // TODO: nop_mux_in1 needs to be the nop[31:0]
+  	wire [31:0] nop_mux_in0;
 	wire [31:0] nop_mux_out;
 	TWO_INPUT_MUX nop_mux (
 		.sel(nop_mux_sel),
 		.in0(nop_mux_in0),
 		.in1(32'b0000_0000_0000_0000_0000_0000_0001_0011), // nop_mux_in1 = addi x0, x0, 0 = 00000000000000000000000000010011
 		.out(nop_mux_out)
+	);
+
+	wire rs1_mux_sel;
+  	wire [31:0] rs1_mux_in0, rs1_mux_in1;
+	wire [31:0] rs1_mux_out;
+	TWO_INPUT_MUX rs1_mux (
+		.sel(rs1_mux_sel),
+		.in0(rs1_mux_in0),
+		.in1(rs1_mux_in1),
+		.out(rs1_mux_out)
+	);
+
+	wire rs2_mux_sel;
+  	wire [31:0] rs2_mux_in0, rs2_mux_in1;
+	wire [31:0] rs2_mux_out;
+	TWO_INPUT_MUX rs2_mux (
+		.sel(rs2_mux_sel),
+		.in0(rs2_mux_in0),
+		.in1(rs2_mux_in1),
+		.out(rs2_mux_out)
 	);
 
     wire [31:0] pc_decode_register_d;
@@ -182,21 +202,301 @@ module cpu #(
 
     wire [31:0] instruction_decode_register_d;
     reg [31:0] instruction_decode_register_q;
-    always (@posedge clk) begin
+    always @(posedge clk) begin
         instruction_decode_register_q <= instruction_decode_register_d;
     end
+
+    wire [31:0] rs1_register_d;
+    reg [31:0] rs1_register_q;
+    always @(posedge clk) begin
+        rs1_register_q <= rs1_register_d;
+    end
+
+    wire [31:0] rs2_register_d;
+    reg [31:0] rs2_register_q;
+    always @(posedge clk) begin
+        rs2_register_q <= rs2_register_d;
+    end
+
+	// jal_adder
+    wire [31:0] jal_adder_in0, jal_adder_in1;
+    wire [31:0] jal_adder_out;
+	ADDER jal_adder (
+		.in0(jal_adder_in0),
+		.in1(jal_adder_in1),
+		.out(jal_adder_out)
+	);
 	
 	// Wiring for D stage
-    assign pc_decode_register_d = pc_register_q; // for pc pipeline register in decode stage
-
-	assign bios_douta = pc_thirty_mux_in0;
-	assign imem_doutb = pc_thirty_mux_in1;
+	assign pc_thirty_mux_in0 = bios_douta;
+	assign pc_thirty_mux_in1 = imem_doutb;
 	
-	assign pc_thirty_mux_out = nop_mux_in0;
+	assign nop_mux_in0 = pc_thirty_mux_out;
+
+    assign pc_decode_register_d = pc_register_q; // for pc pipeline register in decode stage
+	assign instruction_decode_register_d = nop_mux_out; // for instruction pipeline register in decode stage
 
     // wiring to regfile
     assign ra1 = nop_mux_out[19:15];
     assign ra2 = nop_mux_out[24:20];
 
+	assign jal_adder_in0 = pc_register_q;
+	assign jal_adder_in1 = {{20{nop_mux_out[31]}}, nop_mux_out[19:12], nop_mux_out[20], nop_mux_out[30:21], 1'b0};
+
+	assign pc_mux_in1 = jal_adder_out;
+
+	assign rs1_mux_in0 = rd1;
+	assign rs2_mux_in0 = rd2;
+
+	assign rs1_register_d = rs1_mux_out;
+	assign rs2_register_d = rs2_mux_out;
+
 	// 3. X -------------------------------------------------------
+
+    wire [31:0] instruction_execute_register_d;
+    reg [31:0] instruction_execute_register_q;
+    always @(posedge clk) begin
+        instruction_execute_register_q <= instruction_execute_register_d;
+    end
+
+    // PC Pipeline Register Execute Stage
+    wire [31:0] pc_execute_register_d;
+    reg [31:0] pc_execute_register_q;
+    always @(posedge clk) begin
+        pc_execute_register_q <= pc_execute_register_d;
+    end
+
+    // ALU Pipeline Register
+    wire [31:0] alu_register_d;
+    reg [31:0] alu_register_q;
+    always @(posedge clk) begin
+        alu_register_q <= alu_register_d;
+    end
+
+	// imm_gen
+	wire [31:0] imm_gen_in;
+	wire [31:0] imm_gen_out;
+	IMM_GEN imm_gen (
+		.inst(imm_gen_in), 
+		.imm(imm_gen_out)
+	);
+
+    // RS1_MUX2
+	wire [1:0] rs1_mux2_sel;
+  	wire [31:0] rs1_mux2_in0, rs1_mux2_in1, rs1_mux2_in2;
+	wire [31:0] rs1_mux2_out;
+	FOUR_INPUT_MUX rs1_mux2 (
+		.sel(rs1_mux2_sel),
+		.in0(rs1_mux2_in0),
+		.in1(rs1_mux2_in1),
+		.in2(rs1_mux2_in2),
+		.in3(0),
+		.out(rs1_mux2_out)
+	);
+
+    // RS2_MUX2
+	wire [1:0] rs2_mux2_sel;
+  	wire [31:0] rs2_mux2_in0, rs2_mux2_in1, rs2_mux2_in2;
+	wire [31:0] rs2_mux2_out;
+	FOUR_INPUT_MUX rs2_mux2 (
+		.sel(rs2_mux2_sel),
+		.in0(rs2_mux2_in0),
+		.in1(rs2_mux2_in1),
+		.in2(rs2_mux2_in2),
+		.in3(0),
+		.out(rs2_mux2_out)
+	);
+
+    // Branch Comparator
+	wire [31:0] branch_comp_rs1;
+	wire [31:0] branch_comp_rs2;
+	wire branch_comp_br_un;
+	wire branch_comp_br_eq;
+    wire branch_comp_br_lt;
+	BRANCH_COMPARATOR branch_comp (
+		.rs1(branch_comp_rs1),
+		.rs2(branch_comp_rs2),
+		.br_un(branch_comp_br_un),
+		.br_eq(branch_comp_br_eq),
+		.br_lt(branch_comp_br_lt)
+	);
+
+    // A_MUX
+    wire [1:0] a_mux_sel;
+    wire [31:0] a_mux_in0, a_mux_in1, a_mux_in2;
+    wire [31:0] a_mux_out;
+    FOUR_INPUT_MUX a_mux (
+		.sel(a_mux_sel),
+		.in0(a_mux_in0),
+		.in1(a_mux_in1),
+		.in2(a_mux_in2),
+		.in3(0),
+		.out(a_mux_out)
+	);
+
+    // B_MUX
+    wire [1:0] b_mux_sel;
+    wire [31:0] b_mux_in0, b_mux_in1;
+    wire [31:0] b_mux_out;
+    TWO_INPUT_MUX b_mux (
+		.sel(b_mux_sel),
+		.in0(b_mux_in0),
+		.in1(b_mux_in1),
+		.out(b_mux_out)
+	);
+
+    // ALU
+    wire [3:0] alu_sel;
+    wire [31:0] alu_rs1, alu_rs2;
+    wire [31:0] alu_out;
+    ALU alu (
+        .alu_sel(alu_sel), 
+        .rs1(alu_rs1), 
+        .rs2(alu_rs2), 
+        .out(alu_out)
+    );
+
+    // CSR
+    wire csr_mux_sel;
+    wire [31:0] csr_mux_in0, csr_mux_in1;
+    wire [31:0] csr_mux_out;
+    TWO_INPUT_MUX csr_mux (
+        .sel(csr_mux_sel),
+        .in0(csr_mux_in0),
+        .in1(csr_mux_in1),
+        .out(csr_mux_out)
+    );
+
+    wire [31:0] csr_in;
+    always @(posedge clk) begin
+        tohost_csr <= csr_in;
+    end
+
+    // rs2_mux3 (MUX going to memory from rs2)
+    wire [1:0] rs2_mux3_sel;
+  	wire [31:0] rs2_mux3_in0, rs2_mux3_in1, rs2_mux3_in2;
+	wire [31:0] rs2_mux3_out;
+	FOUR_INPUT_MUX rs2_mux2 (
+		.sel(rs2_mux3_sel),
+		.in0(rs2_mux3_in0),
+		.in1(rs2_mux3_in1),
+		.in2(rs2_mux3_in2),
+		.in3(0),
+		.out(rs2_mux3_out)
+	);
+
+    // UART
+    wire [31:0] uart_instruction, uart_addr;
+    wire [7:0] uart_tx_data_in;
+    wire [31:0] uart_out;
+    IO_MEMORY_MAP uart (
+        .clk(clk),
+        .instruction(uart_instruction),
+        .addr(uart_addr),
+        .uart_tx_data_in(uart_tx_data_in),
+        .out(uart_out)
+    );
+
+    wire [1:0] addr_mux_sel;
+  	wire [31:0] addr_mux_in0, addr_mux_in1, addr_mux_in2, addr_mux_in3;
+	wire addr_mux_out;
+	FOUR_INPUT_MUX addr (
+		.sel(addr_mux_sel),
+		.in0(addr_mux_in0),
+		.in1(addr_mux_in1),
+		.in2(addr_mux_in2),
+		.in3(addr_mux_in3),
+		.out(addr_mux_out)
+	);
+
+    // For addr_mux_sel (output of memories)
+    always @(*) begin
+        case(alu_out[31:28])
+            4'b0001: addr_mux_sel = 2'b01;
+            4'b0011: addr_mux_sel = 2'b01;
+            4'b0100: addr_mux_sel = 2'b00;
+            4'b1000: addr_mux_sel = 2'b10;
+            default: begin
+                addr_mux_sel = 2'b01;
+            end
+        endcase
+    end
+
+	// Wiring for X stage
+	assign instruction_execute_register_d = instruction_decode_register_q;
+	assign imm_gen_in = instruction_decode_register_q;
+
+    // PC Execute Register Input
+    assign pc_execute_register_d = pc_decode_register_q;
+
+    // ALU Pipeline Register Input
+    assign alu_register_d = alu_out;
+
+    // rs1_mux2 inputs
+	assign rs1_mux2_in0 = rs1_register_q;
+	assign rs2_mux2_in0 = rs2_register_q;
+
+    // inputs to branch comparator
+    assign branch_comp_rs1 = rs1_mux2_out;
+    assign branch_comp_rs2 = rs2_mux2_out;
+
+    // inputs to A-MUX
+    assign a_mux_in0 = rs1_mux2_out;
+    assign a_mux_in1 = 0; // temp
+    assign a_mux_in2 = 0; // temp
+    assign a_mux_sel = 0; // temp
+
+    // inputs to B-MUX
+    assign b_mux_in0 = rs2_mux2_out;
+    assign b_mux_in1 = imm_gen_out;
+    assign b_mux_sel = 0; // temp
+
+    // inputs to ALU
+    assign alu_rs1 = a_mux_out;
+    assign alu_rs2 = b_mux_out;
+    assign alu_sel = 0; // temp
+
+    // inputs to CSR_MUX
+    assign csr_mux_in0 = imm_gen_out;
+    assign csr_mux_in1 = rs1_mux2_out;
+    assign csr_mux_sel = 0; // temp
+
+    // input to CSR register
+    assign csr_in = csr_mux_out;
+
+    // inputs to RS2_MUX3
+    assign rs2_mux3_in0 = rs2_mux2_out;
+    assign rs2_mux3_in1 = 0; // temp
+    assign rs2_mux3_in2 = 0; // temp
+
+    // input to DMEM
+    assign dmem_addr = alu_out[15:2];
+    assign dmem_din = rs2_mux3_out;
+    assign dmem_en = 0; // temp
+    assign dmem_we = 4'b1111; // temp
+    // output of dmem = dmem_dout
+
+    // input to BIOS
+    assign bios_addrb = alu_out;
+    assign bios_enb = 0; // temp
+
+    // input to IMEM
+    assign imem_addra = alu_out[15:2];
+    assign dina = rs2_mux3_out;
+    assign imem_wea = 4'b1111; // temp
+    assign imem_ena = 0; // temp
+
+    // input to UART
+    assign uart_instruction = instruction_decode_register_q;
+    assign uart_addr = alu_out;
+    assign uart_tx_data_in = rs2_mux3_out[7:0];
+
+    // addr MUX
+    assign addr_mux_in0 = bios_doutb;
+    assign addr_mux_in1 = dmem_dout;
+    assign addr_mux_in2 = uart_out;
+    assign addr_mux_in3 = 0;
+    
+
+
 endmodule
